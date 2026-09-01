6990e52d0512758ac396be20ac5c39e475a262c7
Andre Monreal-Hernandez <amonreal@example.com>
2026-08-31 18:07:14 -0700

    chore: update author to Andres Monreal-Hernandez and link ORCID 0009-0009-1207-8597 in all citation files

--- catcert/__init__.py
+++ catcert/__init__.py
@@ -4,7 +4,7 @@ and Surface Energy Convergence Certification for Heterogeneous Catalysis & DFT S
 """
 
 __version__ = "1.0.0"
-__author__ = "Andre Monreal-Hernández"
+__author__ = "Andres Monreal-Hernández"
 __license__ = "MIT"
 
 from catcert.core.surface_energy import calculate_surface_energy_convergence, SurfaceEnergyResult
